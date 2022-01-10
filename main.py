0d4d9aebed9e1e94e31fef252621f42df7f7fa97
timos09 <timbosik28@gmail.com>
2022-01-11 01:18:44 +0200

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+
+print("Hi, whats is your name?")
+time.sleep(2)
+x = str(input("Type your name: "))
+time.sleep(2)
+print("Cool! your name is ", x )
+print("Now let's find out how old are you!")
+time.sleep(2)
+c = int(input("Type your age: "))
+print("Amazing, you are", c)

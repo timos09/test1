b75c698b12ffdfd0662d34eae578c95b20aadf2e
timos09 <timbosik28@gmail.com>
2022-01-11 03:05:50 +0200

    2com

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
 import time
 
+def welcome():  
+    print("Hi, whats is your name?")
+    time.sleep(2)
+    x = str(input("Type your name: "))
+    time.sleep(2)
+    print("Cool! your name is ", x )
+    print("Now let's find out how old are you!")
+    time.sleep(2)
+    c = int(input("Type your age: "))
+    print("Amazing, you are", c)
+welcome()
 
-print("Hi, whats is your name?")
-time.sleep(2)
-x = str(input("Type your name: "))
-time.sleep(2)
-print("Cool! your name is ", x )
-print("Now let's find out how old are you!")
-time.sleep(2)
-c = int(input("Type your age: "))
-print("Amazing, you are", c)
+print("Now lets find out where are you from?")
+str(input("Type where are you from: "))
